d77b071d2aa8038a0ba50c7ed0e4ca9b4584fa07
yoon <leeyt1201@gmail.com>
2023-04-22 16:10:50 +0900

    feat: static block initialization singleton

--- creational_patterns/src/singleton/StaticBlockSingleton.java
+++ creational_patterns/src/singleton/StaticBlockSingleton.java
@@ -1,2 +1,21 @@
-package singleton;public class StaticBlockSingleton {
+package singleton;
+
+public class StaticBlockSingleton {
+
+    private static StaticBlockSingleton instance;
+
+    private StaticBlockSingleton() {}
+
+    // static block initialization for exception handling
+    static {
+        try {
+            instance = new StaticBlockSingleton();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception occurred in creating singleton instance");
+        }
+    }
+
+    public static StaticBlockSingleton getInstance() {
+        return instance;
+    }
 }
