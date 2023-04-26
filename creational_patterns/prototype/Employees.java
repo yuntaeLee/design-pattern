b3146586fdb1934b92ea35e3e4784aa42732734c
yoon <leeyt1201@gmail.com>
2023-04-26 16:53:36 +0900

    prototype pattern

---
+++ creational_patterns/prototype/Employees.java
@@ -0,0 +1,31 @@
+package creational_patterns.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employees implements Cloneable {
+    
+    private List<String> empList;
+
+    public Employees() {
+        empList = new ArrayList<String>();
+    }
+
+    public Employees(List<String> empList) {
+        this.empList = empList;
+    }
+
+    public void loadData() {
+        empList.addAll(List.of("Pankaj", "Raj", "David", "Lisa"));
+    }
+
+    public List<String> getEmpList() {
+        return empList;
+    }
+
+    @Override
+	public Object clone() throws CloneNotSupportedException{
+			List<String> temp = new ArrayList<String>(this.empList);
+			return new Employees(temp);
+	}
+}
