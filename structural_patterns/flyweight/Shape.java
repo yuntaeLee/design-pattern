94c42ee0290de52234ee040c3a50e3f578b2fdde
yoon <leeyt1201@gmail.com>
2023-04-28 12:27:07 +0900

    flyweight pattern

---
+++ structural_patterns/flyweight/Shape.java
@@ -0,0 +1,9 @@
+package structural_patterns.flyweight;
+
+import java.awt.Graphics;
+import java.awt.Color;
+
+public interface Shape {
+    
+    public void draw(Graphics g, int x, int y, int width, int height, Color color);
+}
